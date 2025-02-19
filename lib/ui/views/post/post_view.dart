c57459659763ac3b3623b56f3f280670ca8e2dbb
Aidarus Badawy <aidarusbadawy@rocketmail.com>
2025-02-19 23:10:25 +0300

    update posts view

--- lib/ui/views/post/post_view.dart
+++ lib/ui/views/post/post_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_for_education/models/post_model.dart';
 import 'package:food_for_education/ui/common/box_text.dart';
 import 'package:food_for_education/ui/common/ui_helpers.dart';
+import 'package:food_for_education/ui/widgets/post_card.dart';
 import 'package:sizer/sizer.dart';
 import 'package:stacked/stacked.dart';
 
@@ -30,25 +31,61 @@ class PostView extends StackedView<PostViewModel> {
           16,
           colorScheme.onSurface,
         ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            viewModel.goBack();
+          },
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 5.1.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            verticalSpaceSmall,
-            PoppinsText.semiBold(
-              postModel.title!,
-              16,
-              colorScheme.onSurface,
-            ),
-            verticalSpaceSmall,
-            PoppinsText.regular(
-              postModel.body!,
-              14,
-              colorScheme.onSurface,
-            ),
-          ],
+        child: SingleChildScrollView(
+          controller: viewModel.scrollController,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpaceSmall,
+              PoppinsText.semiBold(
+                viewModel.currentPost!.title!,
+                16,
+                colorScheme.onSurface,
+              ),
+              verticalSpaceSmall,
+              PoppinsText.regular(
+                viewModel.currentPost!.body!,
+                14,
+                colorScheme.onSurface,
+              ),
+              verticalSpaceMedium,
+              Divider(
+                color: colorScheme.onSurface,
+                thickness: .6,
+              ),
+              verticalSpaceMedium,
+              PoppinsText.semiBold(
+                'Other Related Posts',
+                16,
+                colorScheme.onSurface,
+              ),
+              verticalSpaceSmall,
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: viewModel.filteredPaginatedList.length,
+                separatorBuilder: (context, index) => verticalSpaceSmall,
+                itemBuilder: (context, index) {
+                  return PostCard(
+                    postModel: viewModel.filteredPaginatedList[index],
+                    onTap: (PostModel post) {
+                      viewModel.updatePost(post);
+                    },
+                  );
+                },
+              ),
+              verticalSpaceMedium,
+            ],
+          ),
         ),
       ),
     );
@@ -59,4 +96,10 @@ class PostView extends StackedView<PostViewModel> {
     BuildContext context,
   ) =>
       PostViewModel();
+
+  @override
+  void onViewModelReady(PostViewModel viewModel) {
+    viewModel.initPostView(postModel);
+    super.onViewModelReady(viewModel);
+  }
 }
